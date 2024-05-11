cf9e96ccb395db238f42331bd2f711c0ad7d6655
Rasho-dk <103439939+Rasho-dk@users.noreply.github.com>
2024-05-11 08:56:09 +0200

    genre-class

---
+++ TheMovieDb.Shared/Models/Genre.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using TheMovieDb.Shared.Models;
+
+namespace TheMovieDb.Shared.Models
+{
+    public class Genre
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
+
+public record GenreResponse
+{
+    [JsonPropertyName("genres")]
+    public required List<Genre> Genres { get; set; }
+}
